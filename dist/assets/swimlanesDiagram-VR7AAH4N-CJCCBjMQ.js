d63d0e5302aa1ce3320ee686aa95eacc18c73b99
savior714 <savior714@gmail.com>
2026-09-02 18:27:19 +0900

    feat(ui): refine frontier emphasis, diagram character, and context hierarchy

---
+++ dist/assets/swimlanesDiagram-VR7AAH4N-CJCCBjMQ.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-HODETNUW-CkiwYt-C.js";import{_ as a}from"./index-BJnovOHT.js";import"./chunk-5VM5RSS4-Dik7cGE1.js";import"./chunk-XXDRQBXY-CXTdeu1Q.js";import"./chunk-POPQ4Y6H-BWQwpCOm.js";import"./chunk-F27PBJKO-CV6USR0U.js";import"./channel-CmerXBMW.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
--- dist/assets/swimlanesDiagram-VR7AAH4N-nxvQnsEs.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-HODETNUW-CZM7X63-.js";import{_ as a}from"./index-B9tHaJGM.js";import"./chunk-5VM5RSS4-2Xjk5gd0.js";import"./chunk-XXDRQBXY-CZmxQ6tT.js";import"./chunk-POPQ4Y6H-BNr8EXUK.js";import"./chunk-F27PBJKO-iAmMa7Iw.js";import"./channel-VVALCay_.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
